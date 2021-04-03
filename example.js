5619054cf51ac73358fb01f9d699c78ae16b1764
ramon <dajoseramon@gmail.com>
2021-04-03 07:35:26 -0600

    Small Fix

--- example.js
+++ example.js
@@ -1,13 +1,31 @@
 var gg = new (require('./client.js'))
 
-gg.Live('kr')
-	.then((json) => {
-		console.log(json)
-	})
-	.catch((error) => {
-		console.error(error)
-	})
+// gg.Live('lan')
+// 	.then((json) => {
+// 		console.log(json)
+// 	})
+// 	.catch((error) => {
+// 		console.error(error)
+// 	})
 
-gg.Live('kr', function(error, data) {
-	console.log(error || data)
-})
+// gg.Live('lan', function(error, data) {
+// 	console.log(error || data)
+// })
+
+//gg.Renew("lan", 1766312, print);
+
+gg.Champions('lan', 'Slayermx117', '11')
+.then(
+	(champs) => {
+		console.log(champs);
+	}
+)
+.catch(
+	(error) => {
+		console.log(error);
+	}
+);
+
+function print(error, data){
+	console.log(error || data);
+}
